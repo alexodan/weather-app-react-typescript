2df5f5a125ab1ecb0edd52d000b6d0ce57a03826
Alexis Diaz <alexdan1993@gmail.com>
2020-07-19 16:24:05 -0300

    Adding and configuring tailwind. Adding first component

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+import "./index.css";
+import Card from "./components/Card";
+
 const App: React.FC = () => {
   return (
     <div className="App">
       <h1>Done!</h1>
+      <Card />
     </div>
   );
 };
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
   entry: "./src/index.tsx",
@@ -25,13 +26,17 @@ module.exports = {
       },
       {
         test: /\.css$/,
-        loader: "css-loader",
+        use: [MiniCssExtractPlugin.loader, "css-loader", "postcss-loader"],
       },
     ],
   },
   plugins: [
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, "public", "index.html"),
+    }),
+    new MiniCssExtractPlugin({
+      filename: "index.css",
+      chunkFilename: "styles.css",
     }),
   ],
 };
